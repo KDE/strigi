455cf7c829762b77c69c9ee1e962b26f981778c5
Jos van den Oever <jos@vandenoever.info>
2011-06-02 14:07:57 +0200

    Add script to create tarball.

---
+++ createtararchive.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# script to create a release tarball for strigi
+# this is needed since the project is split up in separate git moduls
+TAG=0.7.5
+git archive --format=tar --prefix=strigi-$TAG/ -o strigi-$TAG.tar v$TAG
+for f in libstreams libstreamanalyzer strigidaemon strigiclient strigiutils; do
+  cd $f
+  git archive --format=tar --prefix=strigi-$TAG/$f/ -o tmptar v$TAG
+  tar Af ../strigi-$TAG.tar tmptar
+  rm tmptar
+  cd -
+done
+bzip2 -9 strigi-$TAG.tar
