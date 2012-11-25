b09f81845b756132564cc7106d86c01c58875874
Jos van den Oever <jos@vandenoever.info>
2012-11-25 19:10:31 +0100

    Update subprojects

--- change_to_development_git.sh
+++ change_to_development_git.sh
@@ -5,6 +5,13 @@
 # cloned from anongit.kde.org, even if you have cloned via
 # git@git.kde.org (which you should not have done, anyway).
 # See http://community.kde.org/Sysadmin/GitKdeOrgManual#Let_Git_rewrite_URL_prefixes
+# This script assumes that you have the following lines in your .gitconfig file:
+#
+#[url "git://anongit.kde.org/"]
+#    insteadOf = kde:
+#[url "git@git.kde.org:"]
+#    pushInsteadOf = kde:
+#
 
 git submodule sync
 git submodule init
